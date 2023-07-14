db61f5016fb97c85beacfba50e3a478b86709396
Eduardo Passarelli <epassarelli@gmail.com>
2023-07-14 18:24:10 -0300

    Packete UI instalado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,23 @@
-@extends('app')
+@extends('layouts.app')
 
 @section('content')
-    <h1>Welcome</h1>
-@endsection
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"> Dashboard</div>{{-- {{ __('Dashboard') }} --}}
 
-@section('footer')
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
