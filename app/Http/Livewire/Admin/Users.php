a6d65a75c924963a414b795355de698d93e30ee0
rrufino71@gmail.com <rrufino71@gmail.com>
2023-08-23 10:35:18 -0300

    roles

--- app/Http/Livewire/Admin/Users.php
+++ app/Http/Livewire/Admin/Users.php
@@ -4,40 +4,72 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\User;
+use App\Models\Role;
+use App\Models\User_rol;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 
 class Users extends Component
 {
     public $user;
-    public $user_id;
+    public $user_id,$user_nombre_rol,$user_rol_id;
     public $name;
     public $email;
     public $password;
     public $muestraModal     = 'none';
     public $muestraModalPass = 'none';
+    public $muestraModalRoles = 'none';
+    public $muestraModalRole = 'none';
 
-    protected $users;
+    protected $users,$roles,$users_roles;
     protected $listeners = ['delete'];
 
     public function render()
     {
-        $users = User::all();
-        return view('livewire.admin.users', compact('users'))->layout('layouts.adminlte');
+        $this->users = User::all();
+        $this->roles = Role::all();
+        if ($this->user_id) {
+            $this->users_roles = User_rol::select([
+                'users_roles.id',
+                'roles.name as rol_nombre'
+                 ])
+                ->leftJoin('roles','roles.id','=','users_roles.rol_id')
+                ->where('users_roles.user_id','=',$this->user_id)
+                ->get();
+        }
+
+        return view('livewire.admin.users', [
+            'users' => $this->users,
+            'roles' => $this->roles,
+            'users_roles' => $this->users_roles,
+        ])->layout('layouts.adminlte');
     }
 
     protected function rules()
     {
-        if ($this->muestraModalPass == 'none') {
+
+        if ($this->muestraModal == 'block') {
             return [
                 'name' => 'required',
                 'email' => 'required | email',
             ];
         }
-        return [
-            'password' => 'required',
-        ];
+
+        if ($this->muestraModalPass == 'block') {
+                return [
+                    'password' => 'required',
+                ];
+        }
+
+        if ($this->muestraModalRole == 'block') {
+            return [
+                'user_rol_id' => 'required',
+            ];
+        }
+
     }
+
+
 
     protected function messages()
     {
@@ -46,6 +78,7 @@ class Users extends Component
             'email.required' => 'El E-mail del usuario es requerido',
             'email.email' => 'El E-mail no responde al formato de correo electronico',
             'password.required' => 'La password del usuario es requerida',
+            'user_rol_id.required' => 'Debe seleccionar un role',
         ];
     }
 
@@ -150,4 +183,83 @@ class Users extends Component
         // $this->isOpen = true;
         $this->muestraModalPass = 'block';
     }
+
+    public function closeModalRoles()
+    {
+        // $this->isOpen = false;
+        $this->muestraModalRoles = 'none';
+    }
+
+    public function openModalRoles()
+    {
+        // $this->isOpen = true;
+        $this->muestraModalRoles = 'block';
+    }
+
+
+    public function closeModalRole()
+    {
+        // $this->isOpen = false;
+        $this->muestraModalRole = 'none';
+    }
+
+    public function openModalRole()
+    {
+        // $this->isOpen = true;
+        $this->muestraModalRole = 'block';
+    }
+
+
+
+    public function roles($id)
+    {
+        $this->user_id = $id;
+        $this->user_nombre_rol = User::where('id','=',$id)->value('name');
+
+
+        $this->users_roles = User_rol::select([
+            'users_roles.id',
+            'roles.name as rol_nombre'
+             ])
+            ->leftJoin('roles','roles.id','=','users_roles.rol_id')
+            ->where('users_roles.user_id','=',$id)
+            ->get();
+
+        $this->openModalRoles();
+    }
+
+
+    public function newRole()
+    {
+        $this->openModalRole();
+    }
+
+
+    public function storeRole()
+    {
+        $this->validate();
+
+
+        User_rol::updateOrCreate(
+            [
+                'user_id' => $this->user_id,
+                'rol_id' => $this->user_rol_id
+            ],
+            [
+                'user_id' => $this->user_id,
+                'rol_id' => $this->user_rol_id
+            ]
+        );
+
+        $this->closeModalRole();
+        $this->emit('mensajePositivo', ['mensaje' => 'Operacion exitosa']);
+    }
+
+
+    public function deleteRole($id)
+    {
+        User_rol::where('id','=',$id)->delete();
+        $this->emit('mensajePositivo', ['mensaje' => 'Operacion exitosa']);
+    }
+
 }
--- resources/views/livewire/admin/users.blade.php
+++ resources/views/livewire/admin/users.blade.php
@@ -11,15 +11,13 @@
             </div>
         </div>
 
-        <!-- Roles Table -->
-        {{-- <h1>Cantidad {{ $cantidad }}</h1> --}}
         <table class="table table-hover table-bordered mt-3">
             <thead>
                 <tr>
                     <th class="text-center">Id</th>
                     <th class="text-center">Nombre</th>
                     <th class="text-center">E-mail</th>
-                    <th style="width: 10%" class="text-center">Acciones</th>
+                    <th style="width: 15%" class="text-center">Acciones</th>
                 </tr>
             </thead>
             <tbody>
@@ -37,6 +35,9 @@
                             <button wire:click="changepass({{ $user->id }})" class="btn btn-sm btn-info"
                                 data-toggle="modal" data-target="#roleModal" title="Cambia Password"><i
                                     class="fas fa-key" style="color: white "></i></button>
+                            <button wire:click="roles({{ $user->id }})" class="btn btn-sm btn-warning"
+                                data-toggle="modal" data-target="#roleModal" title="Roles"><i
+                                    class="fa fa-bars"></i></button>
                         </td>
                     </tr>
                 @endforeach
@@ -132,4 +133,108 @@
 
             </div>
     @endif
+
+
+
+    @if ($muestraModalRoles == 'block')
+        <!-- Role Form Modal -->
+        <div class="modal fade show" id="roleModal" tabindex="-1" role="dialog" aria-labelledby="roleModalLabel"
+            aria-hidden="true" style="display: {{ $muestraModalRoles }}">
+            {{-- <div class="modal" tabindex="-1" role="dialog" wire:ignore.self> --}}
+
+            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="roleModalLabel">
+                            Roles
+                        </h5>
+                    </div>
+                    <div class="modal-body">
+
+                        <h2>{{ $user_nombre_rol}}</h2>
+                        <div class="col-md-4 text-right float-right mb-1">
+                            <button wire:click="newRole" class="btn btn-success" data-toggle="modal"
+                                data-target="#roleModalRole"><i class="fas fa-plus-circle mr-2"
+                                    style="color: white;"></i>Agregar  Role</button>
+                        </div>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Rol</th>
+                                    <th class="p-2 text-center">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($users_roles as $user_rol)
+                                    <tr>
+                                        <td style="width: 30%;" class="p-1 align-middle">{{ $user_rol->rol_nombre }}
+                                        </td>
+                                        <td style="width: 10%;" class="p-1 text-center">
+                                            <button wire:click="deleteRole({{ $user_rol->id }})"
+                                                class="btn btn-sm btn-danger" title="Eliminar"><i
+                                                    class="fas fa-trash-alt" style="color: white "></i></button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+
+                        <div class="modal-footer">
+                            <button type="button" wire:click="closeModalRoles" class="btn btn-secondary"
+                                data-dismiss="modal">Cerrar</button>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+    @endif
+
+
+    @if ($muestraModalRole == 'block')
+    <!-- Role Form Modal -->
+    <div class="modal fade show" id="roleModalRole" tabindex="-1" role="dialog" aria-labelledby="roleModalRoleLabel"
+        aria-hidden="true" style="display: {{ $muestraModalRole }}">
+        {{-- <div class="modal" tabindex="-1" role="dialog" wire:ignore.self> --}}
+
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="roleModalLabel">
+                        Crear Nuevo Role
+                    </h5>
+                </div>
+                <div class="modal-body">
+
+                    <label>Role <span class="text-danger">*</span></label>
+                    <select class="form-control" id="user_rol_id" wire:model="user_rol_id">
+                        <option value="0">
+                            Seleccione un Role
+                        </option>
+                        @if ($roles)
+                            @foreach ($roles as $role)
+                                <option value="{{ $role->id }}">
+                                    {{ $role->name }}
+                                </option>
+                            @endforeach
+                        @endif
+                    </select>
+                    @error('user_rol_id')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" wire:click="closeModalRole" class="btn btn-secondary"
+                        data-dismiss="modal">Cerrar</button>
+                        <button wire:click="storeRole" class="btn btn-primary">Guardar</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endif
+
 </div>
