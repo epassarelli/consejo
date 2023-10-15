97b01019807815161a6603b3132ae08d728d6bf9
Diego Vidal <diego.vidal@rec.uba.ar>
2023-10-15 12:36:41 -0300

    modify status data

--- database/seeders/FacultadesTableSeeder.php
+++ database/seeders/FacultadesTableSeeder.php
@@ -16,97 +16,97 @@ class FacultadesTableSeeder extends Seeder
         Facultades::create([
             'id'=> 1,
             'name'=> 'Facultad de Arquitectura Diseño y Urbanismo',
-            'status'=> 'true'
+            'status'=> true
         ]);
         
         Facultades::create([
             'id'=> 2,
             'name'=> 'Facultad de Cs Exactas y Naturales',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 3,
             'name'=> 'Facultad de Filosofía y Letras',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 4,
             'name'=> 'Facultad de Ingeniería ',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 5,
             'name'=> 'Facultad de Farmacia y Bioquímica',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 6,
             'name'=> 'Facultad de Derecho',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 7,
             'name'=> 'Facultad de Agronomía',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 8,
             'name'=> 'Facultad de Cs Veterinarias',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 9,
             'name'=> 'Facultad de Cs Económicas',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 10,
             'name'=> 'Facultad de Cs Sociales',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 11,
             'name'=> 'Facultad de Medicina',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 12,
             'name'=> 'Facultad de Psicología',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 13,
             'name'=> 'Facultad de Odontología',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 14,
             'name'=> 'Escuela Superior de Comercio Carlos Pellegrini',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 15,
             'name'=> 'CBC',
-            'status'=> 'true'
+            'status'=> true
         ]);
 
         Facultades::create([
             'id'=> 16,
-            'name'=> 'Colegio Nacional de Buenos Aires',
-            'status'=> 'true'
+          'name'=> 'Colegio Nacional de Buenos Aires',
+            'status'=> true
         ]);
 
     }
