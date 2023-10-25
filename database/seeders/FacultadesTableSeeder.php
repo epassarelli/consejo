f5bd2882430b803c34976819c67cde5bcd402e8f
Diego Vidal <diego.vidal@rec.uba.ar>
2023-10-25 00:46:42 -0300

    se modifica facultades por facultad

--- database/seeders/FacultadesTableSeeder.php
+++ database/seeders/FacultadesTableSeeder.php
@@ -13,7 +13,7 @@ class FacultadesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        Facultades::create([
+        Facultad::create([
             'id' => 1,
             'name' => 'Facultad de Arquitectura Diseño y Urbanismo',
             'status' => 1,
@@ -21,7 +21,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 2,
             'name' => 'Facultad de Cs Exactas y Naturales',
             'status' => 1,
@@ -29,7 +29,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 3,
             'name' => 'Facultad de Filosofía y Letras',
             'status' => 1,
@@ -37,7 +37,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 4,
             'name' => 'Facultad de Ingeniería ',
             'status' => 1,
@@ -45,7 +45,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 5,
             'name' => 'Facultad de Farmacia y Bioquímica',
             'status' => 1,
@@ -53,7 +53,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 6,
             'name' => 'Facultad de Derecho',
             'status' => 1,
@@ -61,7 +61,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 7,
             'name' => 'Facultad de Agronomía',
             'status' => 1,
@@ -69,7 +69,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 8,
             'name' => 'Facultad de Cs Veterinarias',
             'status' => 1,
@@ -77,7 +77,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 9,
             'name' => 'Facultad de Cs Económicas',
             'status' => 1,
@@ -85,7 +85,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 10,
             'name' => 'Facultad de Cs Sociales',
             'status' => 1,
@@ -93,7 +93,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 11,
             'name' => 'Facultad de Medicina',
             'status' => 1,
@@ -101,7 +101,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 12,
             'name' => 'Facultad de Psicología',
             'status' => 1,
@@ -109,7 +109,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 13,
             'name' => 'Facultad de Odontología',
             'status' => 1,
@@ -117,7 +117,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 14,
             'name' => 'Escuela Superior de Comercio Carlos Pellegrini',
             'status' => 1,
@@ -125,7 +125,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 15,
             'name' => 'CBC',
             'status' => 1,
@@ -133,7 +133,7 @@ class FacultadesTableSeeder extends Seeder
             'updated_at' => now(),
         ]);
 
-        Facultades::create([
+        Facultad::create([
             'id' => 16,
             'name' => 'Colegio Nacional de Buenos Aires',
             'status' => 1,
