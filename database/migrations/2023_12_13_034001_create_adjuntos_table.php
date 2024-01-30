ad8e73971388ca812accdc5a60e6f5eb7f0f4fc5
Karina <karuvargas@gmail.com>
2024-01-30 16:17:58 -0300

    agregué campo pdf a sesiones, la ejecucion de un seeder y migration de archivos

---
+++ database/migrations/2023_12_13_034001_create_adjuntos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        
+        Schema::create('adjuntos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('items_temario_id')->nullable();
+            $table->foreign('items_temario_id')->references('id')->on('items_temario')->restrictOnDelete();
+            $table->string('titulo')->nullable();
+            $table->string('archivo');
+            $table->unsignedBigInteger('usuarioAlta_id')->nullable();
+            $table->foreign('usuarioAlta_id')->references('id')->on('users')->restrictOnDelete();
+            $table->unsignedBigInteger('usuarioModifico_id')->nullable();
+            $table->foreign('usuarioModifico_id')->references('id')->on('users')->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('adjuntos');
+    }
+};
